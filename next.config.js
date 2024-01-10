bfb5090aa9aaee1f7eca451f3e9a973a3e1c358f
jacoby <jmigwambo@hotmail.com>
2024-01-10 14:42:34 +0200

    stripe webhooks next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "misc.scdn.co",
+            },
+            {
+                protocol: "https",
+                hostname: "i.scdn.co",
+            },
+            {
+                protocol: "https",
+                hostname: "geo-media.beatsource.com",
+            },
+            {
+                protocol: "https",
+                hostname: "i1.sndcdn.com",
+            },
+            {
+                protocol: "https",
+                hostname: "seed-mix-image.spotifycdn.com",
+            },
+            {
+                protocol: "https",
+                hostname: "media.pitchfork.com",
+            },
+            {
+                protocol: "https",
+                hostname: "tbvswgcbrilqlaxdsxii.supabase.co",
+            }
+        ],
+
+    },
+}
 
 module.exports = nextConfig
